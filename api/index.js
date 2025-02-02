ab2d64ec1fe1582dd8d89f174a30fb861d376473
yatharth-7022 <yatharth7022@gmail.com>
2025-02-02 21:18:10 +0530

    creating test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./Routes/user.route.js";
 dotenv.config();
 
 mongoose
@@ -17,3 +18,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
+
+app.use("/api/user", userRouter);
