8e806161ac1906ed6148c5e518055a01320b4d4e
yatharth-7022 <yatharth7022@gmail.com>
2025-02-03 08:13:08 +0530

    changing the dependencies for tailwind and downgrading it from v4 to v3

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
