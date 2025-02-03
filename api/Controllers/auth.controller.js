238cd26fac707f81a398b209ebfea44294301867
yatharth-7022 <yatharth7022@gmail.com>
2025-02-03 07:05:24 +0530

    creating middleware and function to handle errors

---
+++ api/Controllers/auth.controller.js
@@ -0,0 +1,11 @@
+import User from "../models/user.model.js";
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const newUser = new User({ username, email, password });
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./Routes/user.route.js";
+import authRouter from "./Routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -14,9 +15,21 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCOde || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
