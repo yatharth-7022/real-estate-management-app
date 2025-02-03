47a9392b9690ea032d1424a2d2df38a07f6b0b9b
yatharth-7022 <yatharth7022@gmail.com>
2025-02-03 16:04:09 +0530

    eveything is working as of now. USer authentication is done. Once user signs up data is sent to database

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,16 +1,51 @@
+import { useState } from "react";
 import { Button } from "../components/ui/button";
 import { Link } from "react-router-dom";
+import { SIGN_UP } from "../../api";
 export const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch(SIGN_UP, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center my-7 font-semibold">Sign Up</h1>
-      <form className="flex flex-col gap-4">
-        <input className="border-2 p-3 " type="text" placeholder="Username" />
-        <input className="border-2 p-3 " type="email" placeholder="Email" />
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          className="border-2 p-3 "
+          id="username"
+          type="text"
+          placeholder="Username"
+          onChange={handleChange}
+        />
+        <input
+          className="border-2 p-3 "
+          id="email"
+          type="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
         <input
           className="border-2 p-3 "
           type="password"
+          id="password"
           placeholder="Password"
+          onChange={handleChange}
         />
         <div className="flex justify-center">
           <Button variant="secondary">SIGN UP</Button>{" "}
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,18 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// Get the directory name using import.meta.url
+const __dirname = new URL(".", import.meta.url).pathname;
+
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
